74e7dab7f78350e6751172cab62b9cc3d24c1510
Thirawat Akkasriwon <thirawat.dev@gmail.com>
2025-07-01 10:53:29 +0700

    Add function show/hidden password

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,6 +5,7 @@ import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { LanguageThemeToggleComponent } from '../../shared/language-theme-toggle/language-theme-toggle.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,11 +15,12 @@ import { TranslateModule } from '@ngx-translate/core';
     NzDividerModule,
     NzIconModule,
     LanguageThemeToggleComponent,
-    TranslateModule
+    TranslateModule,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  
+  passwordVisible = false;
 }
--- src/app/shared/icons/icons.ts
+++ src/app/shared/icons/icons.ts
@@ -1,13 +1,11 @@
 import {
   GoogleOutline,
-  LockOutline,
-  MailOutline,
-  UserOutline
+  EyeOutline,
+  EyeInvisibleOutline
 } from '@ant-design/icons-angular/icons';
 
 export const ICONS = [
   GoogleOutline,
-  LockOutline,
-  MailOutline,
-  UserOutline
+  EyeOutline,
+  EyeInvisibleOutline
 ];
