07d8c5ca573468f8e965ccc2f7dc1c87a1b3ba14
Thirawat Akkasriwon <thirawat.dev@gmail.com>
2025-07-01 10:18:11 +0700

    Add UI Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { LanguageThemeToggleComponent } from '../../shared/language-theme-toggle/language-theme-toggle.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
-  imports: [NzInputModule, NzButtonModule],
+  imports: [
+    NzInputModule,
+    NzButtonModule,
+    NzDividerModule,
+    NzIconModule,
+    LanguageThemeToggleComponent,
+    TranslateModule
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  
 }
